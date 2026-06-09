c571f58dcebeaf63065b97e117bb182aaf235341
Ahmed Hashem <ahalnaji002@gmail.com>
2026-06-09 21:20:48 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql2");
 
-// تم تعديل البيانات بربطها مباشرة لتخطي مشكلة الـ .env
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",       // المستخدم الافتراضي لـ phpMyAdmin
-  password: "",       // الباسورد الافتراضي يكون فارغاً في XAMPP
-  database: "healthcare_conflict_system", // 👈 ضع هنا اسم قاعدة البيانات الحقيقي كما هو مكتوب في phpMyAdmin
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
